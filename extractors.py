8927ad74b497ed5b752433c2cb7e12a1a9c13223
Sida Wang <sidaw@cs.stanford.edu>
2015-04-28 16:23:02 -0700

    dreeson solution extractor and mixture of gaussians

--- extractors.py
+++ extractors.py
@@ -5,6 +5,7 @@ Given a moment matrix and its values, extract solutions
 """
 
 import numpy as np
+import scipy as sc
 import scipy.linalg # for schur decomp, which np doesnt have
 import numpy.linalg # for its norm, which suits us better than scipy
 import util
@@ -28,7 +29,7 @@ def extract_solutions_lasserre(MM, ys, Kmax=10, tol=1e-5):
     # now using Lassarre's notation in the extraction section of
     # "Moments, Positive Polynomials and their Applications"
     T,Ut = util.srref(Vs[0:count,:])
-    print 'the next biggest eigenvalue we are losing is %f' % Sigma[count]
+    print 'the biggest singular value we are losing is %f' % Sigma[count]
     # inplace!
     util.row_normalize_leadingone(Ut)
 
@@ -62,6 +63,50 @@ def extract_solutions_lasserre(MM, ys, Kmax=10, tol=1e-5):
         sols[var] = [quadf(Ns[var], Q[:,j]) for j in range(bl)]
     #ipdb.set_trace()
     return sols
+
+def extract_solutions_dreesen_proto(MM, ys, Kmax=10, tol=1e-5):
+    """
+    extract solutions dreesen's nullspace method
+    this is the prototype implementation that does not match solutions!
+    """
+    M = MM.numeric_instance(ys)
+    Us,Sigma,Vs=sc.linalg.svd(M)
+    
+    count = min(Kmax,sum(Sigma>tol))
+    Z = Us[:,0:count]
+    print 'the next biggest eigenvalue we are losing is %f' % Sigma[count]
+
+    dict_row_monos = dict_mono_to_ind(MM.row_monos)
+    
+    sols = {}
+    for var in MM.vars:
+        S1 = np.zeros( (len(MM.row_monos), len(MM.row_monos)) )
+        Sg = np.zeros( (len(MM.row_monos), len(MM.row_monos)) )
+        # a variable is in basis of the current var if var*basis in row_monos
+        basis = []
+        i = 0
+        for mono in MM.row_monos:
+            if mono*var in MM.row_monos:
+                basis.append(mono)
+                basisind = dict_row_monos[mono]
+                gind = dict_row_monos[mono*var]
+                
+                S1[i, basisind] = 1
+                Sg[i, gind] = 1
+                i += 1
+        S1 = S1[0:i,:]
+        Sg = Sg[0:i,:]
+        
+        A = Sg.dot(Z)
+        B = S1.dot(Z)
+        
+        # damn this, cant i just have a GE solver that works for non-square matrices?
+        __,__,P = np.linalg.svd(np.random.randn(count,i), full_matrices = False)
+        
+        sols[var] = sc.real(sc.linalg.eigvals(P.dot(A),P.dot(B))).tolist()
+        
+    return sols
+
 
 def test_solution_extractors():
     import sympy as sp
